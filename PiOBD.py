89866a8d4fdf064fa46075b432c6a47942ce50d5
Marcinno <marrcinek96@gmail.com>
2018-11-17 10:47:29 +0100

    test modules and tcp connection

--- PiOBD.py
+++ PiOBD.py
@@ -1,5 +1,16 @@
 #PiOBD
 from libs import tcp_connection #async connection modules contains send funcs
+import threading
+import asyncio
+import time
 
+dataJson = {
+    'A' : 1,
+    'B' : 2,
+}
 if __name__ == "__main__":
+    while True:
+        asyncio.run(tcp_connection.sendData(dataJson))
+        time.sleep(1) #testing loop
+
     print("Elo")    
--- libs/tcp_connection.py
+++ libs/tcp_connection.py
@@ -3,15 +3,6 @@ import json
 import asyncio
 from threading import Thread
 
-#testing section
-P = 1
-C = 3
-dataJson = {
-    '1' : P,
-    '2' : C,
-}
-#testing section
-
 async def sendData(dataJson):
     reader, writer = await asyncio.open_connection('127.0.0.1',5005)
     
